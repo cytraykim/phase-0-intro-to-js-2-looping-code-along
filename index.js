b361f9a603610d9411495aca4ba8aaec4d405f95
Tracy <tracy.kagiri@moringa.student.com>
2023-07-01 23:39:38 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+
+const people = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(namesArr, eventName){
+    const messages=[]
+
+    for(let i=0;i<namesArr.length;i++){
+        messages.push(`Thank you, ${people[i]}, for the wonderful ${eventName} gift!`)
+        console.log(messages);
+    }
+    return messages;
+}
+
+writeCards(people, "surprise")
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num)
+        num--
+    }
+    
+}
+countdown()
